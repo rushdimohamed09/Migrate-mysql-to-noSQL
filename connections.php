4124e7c91de4e0c0301a45dd862a9d48b70a395f
Rushdi Mohamed <rushdimohamed09@gmail.com>
2021-06-15 19:41:52 +0530

    Add files via upload

---
+++ connections.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername="localhost";
+$username="username";
+$password="user_pwd";
+$database="db_name";
+$conn = new mysqli($servername,$username,$password,$database);
+if($conn->connect_error) {
+  exit('Error connecting to database'); //Should be a message a typical user could understand in production
+}
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn->set_charset("utf8mb4");
+
+?>
